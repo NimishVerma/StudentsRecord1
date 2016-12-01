c87f98d32a16e634e28eaf5c1d37911c5ad1902e
bigpythonimish <nimishverma@ymail.com>
2016-12-01 15:43:55 +0530

    Update Sort.py
    removed the fixed number of students.
    added printing the dictionary option.
    might need some better ways to print dictionary.

--- Sort.py
+++ Sort.py
@@ -3,11 +3,14 @@ class Student(object):
         self.name = raw_input(str("Enter name:"))
         self.rollno = int(raw_input("Enter roll no:"))
         self.section = raw_input("Enter section:")
-        
+    def return_class_variables(Student):
+        return(Student.__dict__)
+num = int(raw_input("enter number of students"))
 student_list = {}
-for i in range(10):
+for i in range(num):
     student_list[i] = Student()
-for i in range(10):
-    if student_list[i].rollno > student_list[i+1].rollno:
-        student_list[i], student_list[i+1] = student_list[i+1], student_list[i]
-    
+for j in range(num-1):
+    if student_list[j].rollno > student_list[j+1].rollno:
+        student_list[j], student_list[j+1] = student_list[j+1], student_list[j]
+for k in range(num):
+    print Student.return_class_variables(student_list[k])
